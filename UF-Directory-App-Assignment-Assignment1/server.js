72796fdee31cde7e254d163183da7d2f987f2603
AlessandroAlessandro <alexalario@ufl.edu>
2018-09-04 22:06:55 -0400

    Update server.js

--- UF-Directory-App-Assignment-Assignment1/server.js
+++ UF-Directory-App-Assignment-Assignment1/server.js
@@ -9,13 +9,7 @@ var listingData, server;
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: explore the request object and its properties 
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-   */
+ 
 
     if (request.method === "GET" && parsedUrl.path == "/listings") {
         response.statusCode = 200;
@@ -30,10 +24,7 @@ var requestHandler = function(request, response) {
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-   */
+ 
     listingData = data;
     startServer();
 });
